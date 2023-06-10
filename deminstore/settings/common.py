eb2f1d47f056aa5865ce47a0b41dd52aa284d9e4
Mahsa Kamalianaraki <mahsa.kamalianaraki@gmail.com>
2023-06-09 23:10:20 -0500

    Correct the problem of BASE_DIR in settings

--- deminstore/settings/common.py
+++ deminstore/settings/common.py
@@ -15,10 +15,6 @@ django.utils.encoding.smart_text = smart_str
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
