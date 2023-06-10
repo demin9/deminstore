c6440acb78b88f1b92fdebb3ca8ef382ef92200f
Mahsa Kamalianaraki <mahsa.kamalianaraki@gmail.com>
2023-06-09 22:09:19 -0500

    Correct the problem of STATIC_ROOT settings

--- deminstore/settings/prod.py
+++ deminstore/settings/prod.py
@@ -37,5 +37,5 @@ EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
 DEFAULT_FROM_EMAIL = os.environ['MAILGUN_SMTP_LOGIN']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static/files/'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/files/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
